6044fcf22f8b00f87e58b6cab8e70b21ab299d1a
Tomas Ciernik <Tciernik@outlook.com>
2021-09-13 13:11:47 +0100

    app not crashing, navHostFragment and navController initialized correct way

--- app/src/main/java/com/example/foody/MainActivity.kt
+++ app/src/main/java/com/example/foody/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+import androidx.navigation.fragment.NavHostFragment
+
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +24,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //target the navigation layout
-        val navController = findNavController(R.id.navHostFragment)
+        //val navController = findNavController(R.id.navHostFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // so it doesnt crash the app with java illegal something
+        //https://stackoverflow.com/questions/50502269/illegalstateexception-link-does-not-have-a-navcontroller-set
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         // create destination links to each layout fragments  in our bottom navigation
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.recipesFragment,R.id.favoriteRecipesFragment,R.id.foodJokeFragment))
